32f120041267358b68a50a99c88482f5c01ebf39
Lee Gye Yeon <DevelopLee@LeeGyeui-MacBook-Pro.local>
2015-08-04 23:10:42 +0900

    에러가 난다~~~~

--- mongo_arbiter.js
+++ mongo_arbiter.js
@@ -4,32 +4,40 @@ var zookeeper = require('node-zookeeper-client');
 var util = require('./utils');
 
 var root_shard_path = "/shard1";
-var replSet = "Arbiter";
+var replSet = "arbiter"
 
-function exists(client) {
-	client.exists(
-		root_shard_path,
+function exists(client) {	
+  client.exists(root_shard_path,
 		function (event) {
-			console.log('Got event: %s.', event);
+      console.log('Got event: %s.', event);
+			exists(client, path);
 		},
 		function (error, stat) {
-			if (error) console.log('Failed to check existence of node: %s due to: %s.', path, error);
-			if (stat) util.replication(client, root_shard_path, replSet);
-		}
+      if (error) {
+	console.log('Failed to check existence of node: %s due to: %s.', path, error);     
+	return;
+      }
+
+	if(stat) {
+		util.replication(client, root_shard_path, replSet);
+	} else {
+      util.shard(client, root_shard_path);
+		  util.replication(client, root_shard_path, replSet);
+	}
+    }
 	);
 }
 
 exports.start = function () {
 
-	var client = zookeeper.createClient('localhost:2181');
+  var client = zookeeper.createClient('localhost:2181');
 
 	client.once('connected', function () {
-	  exists(client);
-  });
+    exists(client);
+	});
 
 	client.connect();
 
-	function puts(error, stdout, stderr) {sys.puts(stdout)}
 	exec("sudo screen -S mongo_replSet-Arbiter sudo mongod --port 20017 --dbpath /data/db/replSet_Arbiter --replSet Mongo_study --smallfiles --noprealloc --nojournal --logpath /data/db/replSet_Log/mongo-replSet_Arbiter.log", function (err, stdout, stderr) {
 		sys.puts(stdout);
 	});
