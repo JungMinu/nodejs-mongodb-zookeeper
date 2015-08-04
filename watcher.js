eee37b5710fb05abd2d7ecc0734c4af2d7883f9d
Lee Gye Yeon <DevelopLee@LeeGyeui-MacBook-Pro.local>
2015-08-05 00:00:51 +0900

    각 모듈별로 실행...

--- watcher.js
+++ watcher.js
@@ -7,14 +7,12 @@ var replSet_2 = require("./mongo_secondary1");
 var replSet_3 = require("./mongo_secondary2");
 var arbiter_1 = require("./mongo_arbiter");
 
-var root_shard_path = "/shard1";
-
-function listChildren(client) {
+function listChildren(client, path) {
 	client.getChildren(
-		root_shard_path,
+		path,
 		function (event) {
 			console.log('Got watcher event: %s', event);
-			listChildren(client, root_shard_path);
+			listChildren(client, "/shard1");
 		},
 		function (error, children, stat) {
 			if (error) {
@@ -22,7 +20,7 @@ function listChildren(client) {
 				return;
 			}
 
-			console.log('Children of %s are: %j.', root_shard_path, children);
+			console.log('Children of %s are: %j.', path, children);
 
 			var children1 = children.indexOf("replSet1");
 			var children2 = children.indexOf("replSet2");
@@ -30,23 +28,35 @@ function listChildren(client) {
 			var children4 = children.indexOf("arbiter");
 
 			if (children1 == -1) {
-				replSet_1.start();
+				RecoverMongo ("20000", "replSet1", "mongo_replSet1.log");
         console.log("Restart mongod replSet... port: 20000");
 			}
 			if (children2 == -1) {
-				replSet_2.start();
+				RecoverMongo ("30000", "replSet2", "mongo_replSet2.log");
         console.log("Restart mongod replSet... port: 30000");
 			}
 			if (children3 == -1) {
-				replSet_3.start();
+				RecoverMongo ("40000", "replSet3", "mongo_replSet3.log");
         console.log("Restart mongod replSet... port: 40000");
 			}
 			if (children4 == -1) {
-				arbiter_1.start();
+				RecoverArbiter ("20017", "replSet_Arbiter", "mongo-replSet_Arbiter.log");
 	console.log("Restart mongod arbiter... port: 20017");
 			}
 		}
 	);
+}
+
+function RecoverMongo (port, dbpath, log) {
+	exec("sudo mongod --port " + port + " --dbpath /data/db/" + dbpath + " --replSet Mongo_study --smallfiles --oplogSize 128 --logpath /data/db/replSet_Log/" + log, function(err, stdout, stderr) {
+		sys.puts(stdout);
+	});
+}
+
+function RecoverArbiter (port, dbpath, log) {
+	exec("sudo mongod --port " + port + " --dbpath /data/db/" + dbpath + " --replSet Mongo_study --smallfiles --noprealloc --nojournal --logpath /data/db/replSet_Log/" + log, function(err, stdout, stderr) {
+		sys.puts(stdout);
+	});
 }
 
 function WatchAndRecover(client, path) {
@@ -61,7 +71,7 @@ function start() {
 
 	client.once('connected', function () {
 		console.log('Connected to ZooKeeper.');
-		WatchAndRecover(client, root_shard_path);
+		WatchAndRecover(client, "/shard1");
 	});
 
 	client.connect();
