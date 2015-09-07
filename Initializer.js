c665808cacf24e42c92c6f92602da012b95cf9bd
Minwoo Jung <jmwsoft@gmail.com>
2015-09-08 00:31:40 +0900

    Update Initializer.js

--- Initializer.js
+++ Initializer.js
@@ -1,62 +1,23 @@
-var async = require('async');
-var exec = require('child_process').exec;
+var exc = require('child_process').exec;
+var zooArray = [
+        {
+            "host": "localhost:2181",
+            "path": "./zookeeper_cluster/zookeeper1/zookeeper-3.4.6/bin/"
+        },
+        {
+            "host": "localhost:2182",
+            "path": "./zookeeper_cluster/zookeeper2/zookeeper-3.4.6/bin/"
+        },
+        {
+            "host": "localhost:2183",
+            "path": "./zookeeper_cluster/zookeeper3/zookeeper-3.4.6/bin/"
+        }
+    ];
 
-var gk = require("./common");
-var config = gk.config;
-var rsLauncher = gk.rsLauncher;
-var rsStateWatcher = gk.rsStateWatcher.rsStateWatcher;
-var rsStateEventHandler = gk.rsStateWatcher.EventHandler;
+var zoonum = zooArray.length;
 
-var MongoConfig = config.MongoConfig;
-var rsArray = config.rsArray;
-
-var zkConfig = config.zkConfig;
-var zkRsPath = zkConfig.RsPath;
-var zkArray = config.zkArray;
-var QuorumNum = zkArray.length;
-
-var zkHost = zkArray[0].host;
-for (var i = 1; i < zkArray.length; i++) {
-    zkHost = zkHost + "," + zkArray[i].host;
-}
-
-var EventEmitter = require('events').EventEmitter;
-var zkServerStart = new EventEmitter();
-var MongoStart = new EventEmitter();
-
-// zkServer Cluster 실행
-zkServerStart.on('start', function() {
-    for (var i = 0; i < QuorumNum; i++) {
-        exec("sudo " + zkArray[i].path + "zkServer.sh start", function(error, stdout, stderr) {
+for (var i = 0; i < zoonum; i++) {
+        exc("sudo " + zooArray[i].path + "zkServer.sh start", function(error, stdout, stderr) {
             console.log(stdout);
         });
-    }
-});
-
-// ./config/develop.json 에서 지정한 rs member들을 실행 및 해당 Ephemeral Node를 zk Cluster server에 생성
-MongoStart.on('start', function() {
-    var replSetNum = rsArray.length;
-    var ArbiterName = rsArray[replSetNum-1].name;
-
-    for (var i = 0; i < replSetNum; i++) {
-        rsLauncher.start(rsArray[i], zkHost, zkRsPath);
-    }
-});
-
-// Zookeeper cluster server, replica set mongod 실행
-async.series([    
-    function asyncZkServerStart(cb) {
-        zkServerStart.emit('start');
-        cb(null, "zkServer start");
-    },
-    function asyncMongoStart(cb) {
-        MongoStart.emit('start');
-        cb(null, 'mongo start');
-    }
-], function done(error, results) {
-    console.log('error: ', error);
-    console.log('nodejs-mongodb-zookeeper: ', results);
-    // 몽고, Zookeeper server가 모두 실행된 후에 watcher를 부름
-    rsStateWatcher.start(rsArray, rsStateEventHandler, MongoConfig, zkRsPath, zkHost);
-    console.log('Watcher Start');
-});
+}
